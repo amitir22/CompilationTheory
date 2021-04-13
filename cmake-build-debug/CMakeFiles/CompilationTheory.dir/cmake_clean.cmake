e86cd51e3417aa1517d388b43840df36330f6f18
dvirperetz <dvirperetz@gmail.com>
2021-04-13 23:51:24 +0300

    addede scanner

---
+++ cmake-build-debug/CMakeFiles/CompilationTheory.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompilationTheory.dir/HW1/part_a.cpp.o"
+  "CMakeFiles/CompilationTheory.dir/HW1/part_b.cpp.o"
+  "CompilationTheory.exe"
+  "CompilationTheory.pdb"
+  "libCompilationTheory.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CompilationTheory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CompilationTheory.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CompilationTheory.
+# This may be replaced when dependencies are built.
