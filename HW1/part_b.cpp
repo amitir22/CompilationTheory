d87dd09b2d9d44c1a09db03a1c44dd6ec6eb9b92
amitir22 <amitir22@gmail.com>
2021-04-15 00:33:06 +0300

    weird things happen

--- HW1/part_b.cpp
+++ HW1/part_b.cpp
@@ -1,10 +1,22 @@
 #include "tokens.hpp"
+#include <stack>
 
+// so every indentation will be translated to 4 spaces (0x20 ascii)
+#define INDENT_SPACE_COUNT 4
+
+using std::stack;
 
 int main() {
-	int token = yylex();
+    stack<enum tokentype> open_paren_stack;
+    int current_indent = INDENT_SPACE_COUNT * open_paren_stack.size();
+
+    int token = yylex();
 
     while(token) {
+        if (token == LPAREN || token == LBRACE) {
+            open_paren_stack.push(token);
+        }
+
         token = yylex();
         // Your code here
     }
