3573ba49dc529123a5d9e54cca0b2c669633d175
amitir22 <amitir22@gmail.com>
2021-04-17 21:35:30 +0300

    part a supposed to work part b supposed to work updated run_tests.sh to include external tests for part B moved hw1-tests folder into HW1/tests for a more organized environment

--- HW1/run_tests.sh
+++ HW1/run_tests.sh
@@ -14,6 +14,13 @@ dos2unix -q tests/*.in tests/*.out
 ./part_b.out < tests/b3.in > tests/b3.actual
 ./part_b.out < tests/b4.in > tests/b4.actual
 ./part_b.out < tests/b5.in > tests/b5.actual
+./part_b.out < tests/b_external1.in > tests/b_external1.actual
+./part_b.out < tests/b_external2.in > tests/b_external2.actual
+./part_b.out < tests/b_external3.in > tests/b_external3.actual
+./part_b.out < tests/b_external4.in > tests/b_external4.actual
+./part_b.out < tests/b_external5.in > tests/b_external5.actual
+./part_b.out < tests/b_external6.in > tests/b_external6.actual
+./part_b.out < tests/b_external7.in > tests/b_external7.actual
 
 # diffs part a
 diff tests/ta1.out tests/ta1.actual
@@ -22,8 +29,10 @@ diff tests/ta2.out tests/ta2.actual
 # diffs part b
 diff tests/tb1.out tests/tb1.actual
 diff tests/tb2.out tests/tb2.actual
-diff tests/b1.out tests/b1.actual
-diff tests/b2.out tests/b2.actual
-diff tests/b3.out tests/b3.actual
-diff tests/b4.out tests/b4.actual
-diff tests/b5.out tests/b5.actual
+diff tests/b_external1.out tests/b_external1.actual
+diff tests/b_external2.out tests/b_external2.actual
+diff tests/b_external3.out tests/b_external3.actual
+diff tests/b_external4.out tests/b_external4.actual
+diff tests/b_external5.out tests/b_external5.actual
+diff tests/b_external6.out tests/b_external6.actual
+diff tests/b_external7.out tests/b_external7.actual
