4a8dd35916b04f2d087babbf848bf7a09fecee4a
amitir22 <amitir22@gmail.com>
2021-05-22 22:39:42 +0300

    HW3 zip script in bash (may need to change $() to ${})

---
+++ HW3/zip_hw3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ID1='315836569'
+ID2='307866269'
+ZIP_NAME='$(ID1)-$(ID2).zip'
+
+C_FILES='*.c'
+_PP_FILES='*.%pp'
+CXX_FILES='*.cxx'
+HEADERS='*.h*'
+LEX_FILE='scanner.lex'
+EXE_FILE='hw3'
+FLEX_OUTPUT_FILE='lex.yy.c'
+BISON_OUTPUT_FILE='parser.tab.*pp'
+MAKEFILE='Makefile'
+
+FILES='$(C_FILES) $(_PP_FILES) $(CXX_FILES) $(HEADERS) $(LEX_FILE) $(EXE_FILE) $(FLEX_OUTPUT_FILE)\
+ $(BISON_OUTPUT_FILE) $(MAKEFILE)'
+
+zip $(ZIP_NAME) $(FILES)
