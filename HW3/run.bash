28f3dc75b953a19c2cc7132bccff8a6d5f03a1ed
amitir22 <amitir22@gmail.com>
2021-06-03 01:16:12 +0300

    updated tests, everything still works.

--- HW3/run.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for t in {1..60}
-do ./run_tests.bash $t
-done
--- HW3/run_tests.bash
+++ HW3/run_tests.bash
@@ -1,6 +1,32 @@
-part=$1
-test_=$2
+#!/bin/bash
 
-./hw3 < hw3-tests/t$part.in 2>&1 > hw3-tests/t$part.res
-echo "TEST $part$test_"
-diff hw3-tests/t$part.res hw3-tests/t$part.out
+for num in {1..60}
+do
+  ./hw3 < "more_tests/t$num.in" &> "more_tests/t$num.res"
+  result="FAILED"
+
+  if [$(diff "more_tests/t$num.res" "more_tests/t$num.out") != ""]
+  then
+    result="SUCCESS"
+  fi
+
+  echo "TEST $num: $result"
+
+done
+
+echo ''
+echo "YO TESTS:"
+
+for num in {1..3}
+do
+  ./hw3 < "yo_tests/oy$num.in" &> "yo_tests/oy$num.res"
+
+  result="FAILED"
+
+  if [$(diff "yo_tests/oy$num.res" "yo_tests/oy$num.out") != ""]
+  then
+    result="SUCCESS"
+  fi
+
+  echo "TEST YO $num: $result"
+done
