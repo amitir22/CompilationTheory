ef6875ce77c3193edd4e48b6e036c7dcd8d7e407
amitir22 <amitir22@gmail.com>
2021-06-03 21:43:15 +0300

    done:  - rename data-members  - re-organize declarations/definitions  - rename parameters-names  - rewrite parts of the code  - rename local variables  - added tests to the branch todo:  - replace all comments i didn't write

---
+++ HW3/run_tests.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "TESTS:"
+for num in {1..60}
+do
+  ./hw3 < "tests/t$num.in" &> "more_tests/t$num.res"
+  result="FAILED"
+
+  if [$(diff "tests/t$num.res" "more_tests/t$num.out") != ""]
+  then
+    result="SUCCESS"
+  fi
+
+  echo "TEST $num: $result"
+
+done
+echo ''
+
+echo "YO TESTS:"
+
+for num in {1..3}
+do
+  ./hw3 < "oy_tests/oy$num.in" &> "yo_tests/oy$num.res"
+
+  result="FAILED"
+
+  if [$(diff "oy_tests/oy$num.res" "yo_tests/oy$num.out") != ""]
+  then
+    result="SUCCESS"
+  fi
+
+  echo "TEST YO $num: $result"
+done
+echo ''
